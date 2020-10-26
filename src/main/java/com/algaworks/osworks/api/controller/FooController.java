349fbd945f5589bebc3f5567fb60d25eaed76427
Alessandro Moreira <alessandro.moreira@gmail.com>
2020-10-26 18:39:43 -0300

    exemplificar o básico
    criar um controller e expor um serviço, mapeando uma rota.

---
+++ src/main/java/com/algaworks/osworks/api/controller/FooController.java
@@ -0,0 +1,34 @@
+package com.algaworks.osworks.api.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.algaworks.osworks.domain.model.Foo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * FooController
+ * Controlador de exemplo para demonstrar como expor uma interface.
+ */
+@RestController
+public class FooController {
+
+  @GetMapping("/foos")
+  public List<Foo> listar() {
+    var foo1 = new Foo();
+    foo1.setId(1L);
+    foo1.setNome("Alessandro");
+    foo1.setEmail("ale@email.com");
+    foo1.setTelefone("859992147041");
+
+    var foo2 = new Foo();
+    foo2.setId(2L);
+    foo2.setNome("Leila");
+    foo2.setEmail("leila@email.com");
+    foo2.setTelefone("85999998888");
+    
+    return Arrays.asList(foo1, foo2);
+  }
+}
